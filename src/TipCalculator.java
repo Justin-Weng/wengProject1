c9c7a63856b3d40ae11428443ce6a6a231f47a42
bt_1n3_25 <sample@bths.edu>
2023-09-21 10:15:08 -0400

    Added a while loop

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -6,7 +6,19 @@ public class TipCalculator {
         System.out.println("Welcome to the tip calculator!");
         System.out.print("Enter the number of people in your group: ");
         int people = scan.nextInt();
-        System.out.println("Enter the tip percentage (0-100): ");
+        System.out.print("Enter the tip percentage (0-100): ");
         int tipPercentage = scan.nextInt();
+        double totalCost = 0;
+        while (true) {
+            System.out.print("Enter the item cost (-1 to end): ");
+            double itemCost = scan.nextDouble();
+            if (itemCost == -1) {
+                break;
+            } else {
+                totalCost += itemCost;
+            }
+        }
+        System.out.println("--------------------------------------");
+        System.out.println(totalCost);
     }
 }
